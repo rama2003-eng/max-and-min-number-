0fa9b52b8a68ea570c47c7363d637625c5f0e579
RAMAPRABA J <124386294+rama2003-eng@users.noreply.github.com>
2023-07-14 19:11:30 +0530

    Add files via upload
    python program

---
+++ maxmin.py
@@ -0,0 +1,7 @@
+listlang = []
+numbers = int(input('enter the number of items in list '))
+for num in range(numbers):
+    item = int(input('Entered number '))
+    listlang.append(item)
+print('entered list =',listlang)
+print("Max number = :", max(listlang), "\nMin number :", min(listlang))
